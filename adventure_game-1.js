e55601003e76796c10103f7131c8da30ae36750a
Rens Gouman <99030922@mydavinci.nl>
2017-11-17 12:15:42 +0100

    step 1.1

--- adventure_game-1.js
+++ adventure_game-1.js
@@ -1,4 +1,5 @@
 console.log("hallo");
 
-document.getElementById('pic').src = "bedroom.png";
-document.getElementById('ul-status').removeChild(1);
+//document.getElementById('pic').src = "bedroom.png";
+//document.getElementById('pic').setAttribute("style", "width:50%; height:50%;");
+//document.getElementById('ul-options').removeChild();
