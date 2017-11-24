710e0c2da1a85debcf83741d4765c9eed70831e1
Rens Gouman <99030922@mydavinci.nl>
2017-11-24 14:29:24 +0100

    1.5 removeChild is working now

--- adventure_game-1.js
+++ adventure_game-1.js
@@ -1,7 +1,52 @@
 console.log("hallo");
-var pic = document.getElementById('pic');
-var status = document.getElementById('ul-status');
-var elements = document.getElementsByClassName('li');
+const title = document.getElementById('title');
+const pic = document.getElementById('pic');
+const story = document.getElementById('p-story');
+const options = document.getElementById('ul-options');
+const status = document.getElementById('ul-status');
+
+const li = document.createElement("LI");
+
+//boolean's
+var hyped = false;
+var energy = false;
+var dressed = false;
+var tired = true;
+var dirty = true;
+var late = true;
+var clean = false;
+var nice_breath = false;
+var clean_teeth = false;
+var know_the_weather = false;
+//numbers
+var relationship = 0;
+var irritated_boss = 0;
+//function's
+function remove(number) {
+	status.removeChild(status.childNodes[number]);
+}
+function functionstart() {
+	pic.src = "img/clock7.png";
+	remove(0);
+	remove(0);
+}
+function functionhyped() {
+	pic.src = "img/clock7.png";
+	remove(0);
+	remove(0);
+	textnode('hyped')
+	replaceChild('bye','hallo',0)
+	hyped = true;
+}
+function textnode (text1) {
+	li.innerHTML = text1;
+	status.appendChild(li);
+}
+function replaceChild(text2,id,number) {
+	li.innerHTML = '<button id='+id+'>'+text2+'</button>';
+	options.replaceChild(li, options.childNodes[number]);
+}
+
 
 //document.getElementById('pic').src = "bedroom.png";
 
@@ -12,16 +57,3 @@ var elements = par.getElementsByTagName('li');
 par.removeChild(elements[0]);*/
 
 //document.getElementById("h3").innerHTML = "hallo";
-
-function start() {
-	pic.src = "img/clock7.png";
-	console.log(elements[0]);
-	console.log(status.childNodes);
-	status.removeChild(childNodes[0]);
-}
-function start2() {
-	pic.src = "img/clock7.png";
-	console.log(elements[0]);
-	status.removeChild(elements[0]);
-	status.innerHTML = "<li>hyped</li>";
-}
